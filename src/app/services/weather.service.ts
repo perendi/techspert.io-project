6544d4973c6b3319f1318835881f9c16bed4f78d
Daniel Matyas Perendi <dmperendi1@sheffield.ac.uk>
2020-04-29 15:28:21 +0100

    changed http to https

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
 
   private id = '194333f5b09188fbda8c4a3bbfea30b2';
-  private url = 'http://api.openweathermap.org/data/2.5/find?lat=52.2053&lon=0.1218&cnt='
+  private url = 'https://api.openweathermap.org/data/2.5/find?lat=52.2053&lon=0.1218&cnt='
 
   constructor(private http: Http) { }
 
